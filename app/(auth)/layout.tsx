28695d29466c38dad2c5b1455e0ad219ea375feb
suhasarali22@gmail.com <suhasarali22@gmail.com>
2025-04-18 22:05:48 +0530

    auth page ui

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
 
-import { isAuthenticated } from "@/lib/actions/auth.action";
+//import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
-  const isUserAuthenticated = await isAuthenticated();
-  if (isUserAuthenticated) redirect("/");
 
   return <div className="auth-layout">{children}</div>;
 };
